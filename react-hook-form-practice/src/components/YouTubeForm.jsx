392c1a873d101d222bae2d82b8f757724e7fac7c
HeroTyphoon <pokeaakarsh@gmail.com>
2026-01-26 00:16:55 +0530

    Displaying Error Messages

--- react-hook-form-practice/src/components/YouTubeForm.jsx
+++ react-hook-form-practice/src/components/YouTubeForm.jsx
@@ -3,7 +3,10 @@ import { DevTool } from "@hookform/devtools";
 
 export const YouTubeForm = () => {
     const form = useForm();
-    const {register, control, handleSubmit} = form;
+    const {register, control, handleSubmit, formState} = form;
+
+    const {errors} = formState;
+    console.log(formState);
 
     const onSubmit = (data) => {
         console.log("form submitted", data);
@@ -14,33 +17,42 @@ export const YouTubeForm = () => {
             <h1>YouTube Form</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username", {
-                    required: {
-                        value: true,
-                        message: "Username is required"
-                    },
-                })} /*name={name} ref={ref} onChange={onChange} onBlur={onBlur}*/ />
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" {...register("username", {
+                        required: {
+                            value: true,
+                            message: "Username is required"
+                        },
+                    })} />
+                    <p className={"error"}>{errors.username?.message}</p>
+                </div>
 
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register("email", {
-                    required: {
-                        value: true,
-                        message: "Username is required"
-                    },
-                    pattern: {
-                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                        message: "Email address is required"
-                    }
-                })} />
+                <div className="form-control">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" {...register("email", {
+                        required: {
+                            value: true,
+                            message: "Email address is required"
+                        },
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid Email Address"
+                        }
+                    })} />
+                    <p className={"error"}>{errors.email?.message}</p>
+                </div>
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel", {
-                    required: {
-                        value: true,
-                        message: "Username is required"
-                    },
-                })} />
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register("channel", {
+                        required: {
+                            value: true,
+                            message: "Channel is required"
+                        },
+                    })} />
+                    <p className={"error"}>{errors.channel?.message}</p>
+                </div>
 
                 <button type="submit">Submit</button>
             </form>
