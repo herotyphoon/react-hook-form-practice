cc08beed0e744342f3542ad98fdbc1dfcbff93a7
HeroTyphoon <pokeaakarsh@gmail.com>
2026-01-25 23:55:30 +0530

    Form Validation

--- react-hook-form-practice/src/components/YouTubeForm.jsx
+++ react-hook-form-practice/src/components/YouTubeForm.jsx
@@ -13,15 +13,34 @@ export const YouTubeForm = () => {
         <div>
             <h1>YouTube Form</h1>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username")} /*name={name} ref={ref} onChange={onChange} onBlur={onBlur}*/ />
+                <input type="text" id="username" {...register("username", {
+                    required: {
+                        value: true,
+                        message: "Username is required"
+                    },
+                })} /*name={name} ref={ref} onChange={onChange} onBlur={onBlur}*/ />
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register("email")} />
+                <input type="email" id="email" {...register("email", {
+                    required: {
+                        value: true,
+                        message: "Username is required"
+                    },
+                    pattern: {
+                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                        message: "Email address is required"
+                    }
+                })} />
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel")}/>
+                <input type="text" id="channel" {...register("channel", {
+                    required: {
+                        value: true,
+                        message: "Username is required"
+                    },
+                })} />
 
                 <button type="submit">Submit</button>
             </form>
